c9616cb16ca35afbc1c2e0f88c5d5754ae96068f
DuyLK <leduyptit@gmail.com>
2019-04-16 15:11:10 +0700

    Release version 1.0.0

---
+++ build/webpack.prod.conf.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack')
+const config = require('./webpack.base.conf')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const path = require('path')
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+
+config.output.filename = 'vue-image-lightbox.min.js'
+config.output.libraryTarget = 'umd'
+config.output.library = 'Lightbox'
+
+config.entry =   path.resolve(__dirname, '../src/components/LightBox.vue')
+
+config.devtool = '#source-map'
+
+config.module.rules.push({
+  test: /\.css$/,
+  use: [
+    MiniCssExtractPlugin.loader,
+    'css-loader',
+  ],
+})
+
+config.plugins = (config.plugins || []).concat([
+  new MiniCssExtractPlugin({
+    filename: 'vue-image-lightbox.min.css'
+  }),
+
+  new UglifyJSPlugin({
+    sourceMap: true,
+    uglifyOptions: {
+      compress: {
+        warnings: false,
+        drop_debugger: true,
+        drop_console: true,
+        global_defs: {
+          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        },
+      },
+
+      output: {
+        comments: false,
+      },
+    },
+  }),
+
+  new OptimizeCSSPlugin({
+    cssProcessorOptions: {
+      safe: true,
+    }
+  }),
+])
+
+module.exports = config
